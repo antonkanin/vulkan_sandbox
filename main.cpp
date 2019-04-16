9659513f9173728855c690e410b270290113d9a0
Anton Kanin <antony.kanin@gmail.com>
2019-04-17 01:34:56 +0300

    picking GPU - work in progress

--- main.cpp
+++ main.cpp
@@ -141,7 +141,44 @@ private:
     {
         createInstance();
         setupDebugMessenger();
+        pickPhysicalDevice();
     }
+
+    void pickPhysicalDevice()
+    {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+        if (deviceCount == 0)
+        {
+            throw std::runtime_error(
+                "failed to find GPUs with Vulkan support!");
+        }
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        for (const auto& device : devices)
+        {
+            if (isDeviceSuitable(device))
+            {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        if (physicalDevice == VK_NULL_HANDLE)
+        {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
+
+        VkPhysicalDeviceProperties deviceProperties;
+        vkGetPhysicalDeviceProperties(device, &deviceProperties);
+
+
+    }
+
+    bool isDeviceSuitable(VkPhysicalDevice device) { return true; }
 
     void mainLoop()
     {
@@ -157,7 +194,7 @@ private:
         {
             DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
         }
-        
+
         vkDestroyInstance(instance, nullptr);
 
         glfwDestroyWindow(window);
@@ -236,6 +273,8 @@ private:
 
     GLFWwindow* window;
     VkInstance  instance;
+
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 };
 
 int main()
