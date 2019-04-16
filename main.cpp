7f2699782b6b23efb849c63e489d759d7dde4300
Anton Kanin <antony.kanin@gmail.com>
2019-04-16 22:54:04 +0300

    instantiating vulkan - work in progress

--- main.cpp
+++ main.cpp
@@ -137,7 +137,11 @@ private:
         return true;
     }
 
-    void initVulkan() { createInstance(); }
+    void initVulkan()
+    {
+        createInstance();
+        setupDebugMessenger();
+    }
 
     void mainLoop()
     {
@@ -166,6 +170,28 @@ private:
 
         window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
     }
+
+    void setupDebugMessenger()
+    {
+        if (!enableValidationLayers)
+            return;
+
+        VkDebugUtilsMessengerCreateInfoEXT createInfo = {};
+        createInfo.sType =
+            VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity =
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType =
+            VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+            VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+        createInfo.pUserData       = nullptr; // Optional
+    }
+
+    VkDebugUtilsMessengerEXT debugMessenger;
 
     GLFWwindow* window;
     VkInstance  instance;
