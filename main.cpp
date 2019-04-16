49f2759e52d76d252f187dfe6a2335a5344cc83d
Anton Kanin <antony.kanin@gmail.com>
2019-04-16 17:39:05 +0300

    instantiating vulkan

--- main.cpp
+++ main.cpp
@@ -11,17 +11,76 @@ class HelloTriangleApplication
 public:
     void run()
     {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
-    void initVulkan() {}
+    void createInstance()
+    {
+        VkApplicationInfo appInfo  = {};
+        appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName   = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName        = "No Engine";
+        appInfo.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion         = VK_API_VERSION_1_0;
 
-    void mainLoop() {}
+        VkInstanceCreateInfo createInfo = {};
+        createInfo.sType            = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
 
-    void cleanup() {}
+        uint32_t     glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount   = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if (VK_SUCCESS != result)
+        {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+    void initVulkan() { createInstance(); }
+
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup()
+    {
+        vkDestroyInstance(instance, nullptr);
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+
+    void initWindow()
+    {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        // ...
+
+        window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+    }
+
+    GLFWwindow* window;
+    VkInstance  instance;
 };
 
 int main()
